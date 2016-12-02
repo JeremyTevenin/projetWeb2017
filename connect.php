c048324692d48c44a09317bf0d76114723030d13
JeremyTevenin <jeremy_tevenin@hotmail.fr>
2016-12-02 18:36:01 +0100

    Revert "pattern"
    This reverts commit 8de38f9ad6a38e48d161b17bb0d5c9587867c982.

--- connect.php
+++ connect.php
@@ -9,6 +9,7 @@
     session_start();
     
     function connexion() {
+        
         if ( isset($_SESSION['mail']) &&  isset($_SESSION['nom']) && isset($_SESSION['prenom'])) {
             echo "Bienvenue ".$_SESSION['nom']." ".$_SESSION['prenom']." !";
             echo "<a href=\"deconnexion.php\">  Se deconnecter  </a>";
@@ -16,4 +17,5 @@
             echo 'Bienvenue ! <a href="index.php">Se connecter</a> <a href="inscription.php">S\'inscrire ?</a><br/>';
         }
     }
+
 ?>
--- script.js
+++ script.js
@@ -1,157 +1,153 @@
 $(function() {	
-	var xmlhttp = new XMLHttpRequest();
-	var pass = true;
 	var testNom = false;
 	var testPrenom = false;
-
+	
 	var testVille = false;
 	var testDate = false;
-
+	
 	var testMail = false;
+	
+	var testMdp1 = false;
+	var testMdp2 = false;
+	
+	function valider() {
+		if (testNom && testPrenom && testMail && testVille
+				&& testDate && testMdp1 && testMdp2) {
+			$("#envoyer").fadeIn(); 
+		} else {
+			$("#envoyer").fadeOut(); 
+		}
+	}
+	
+	$("#envoyer").fadeOut(); 
 
-	$('#submit').attr('disabled', 'true');
 	$("#nom").keyup(function() {
 		if ($("#nom").val() == "") {
-			$("#nom").css("border-color", "#ff5b5b");
-			$("#nom").next(".erreurNom").show().attr('src', 'cross.png');
-			$(".erreurNom").next(".erreur").show().text("Veuillez entrer votre nom");
+			$("#nom").css("border-color", "#ff5b5b"); 
+			$("#nom").next(".erreur").show().text("Veuillez entrer votre nom");
 			testNom = false; 
+			valider();
 		} else if (!$("#nom").val().match(/^[-a-zA-Zéèç]+$/)) {
 			$("#nom").css("border-color", "#ff5b5b"); 
-			$("#nom").next(".erreurNom").show().attr('src', 'cross.png');
-			$(".erreurNom").next(".erreur").show().text("Veuillez entrer un nom valide");
+			$("#nom").next(".erreur").show().text("Veuillez entrer un nom valide");	
 			testNom = false;
 		} else {
 			$("#nom").css("border-color", "#00ff00"); 
-			$("#nom").next(".erreurNom").show().attr('src', 'tick.png');
-			$(".erreurNom").next(".erreur").hide().text("");
+			$("#nom").next(".erreur").hide().text("");
 			testNom = true;
+			valider();
 		}
 	});
-
+	
 	$("#prenom").keyup(function() {
 		if ($("#prenom").val() == "") {
 			$("#prenom").css("border-color", "#ff5b5b"); 
-			$("#prenom").next(".erreurPrenom").show().attr('src', 'cross.png');
-			$(".erreurPrenom").next(".erreur").show().text("Veuillez entrer votre prénom");
+			$("#prenom").next(".erreur").show().text("Veuillez entrer votre prénom");
 			testPrenom = false; 
+			valider();
 		} else if (!$("#prenom").val().match(/^[-a-zA-Zéèç]+$/)) {
-			$("#prenom").css("border-color", "#ff5b5b");
-			$("#prenom").next(".erreurPrenom").show().attr('src', 'cross.png');
-			$(".erreurPrenom").next(".erreur").show().text("Veuillez entrer un prénom valide");				
+			$("#prenom").css("border-color", "#ff5b5b"); 
+			$("#prenom").next(".erreur").show().text("Veuillez entrer un prénom valide");	
 			testPrenom = false;
 		} else {
 			$("#prenom").css("border-color", "#00ff00"); 
-			$("#prenom").next(".erreurPrenom").show().attr('src', 'tick.png');
-			$(".erreurPrenom").next(".erreur").hide().text("");
+			$("#prenom").next(".erreur").hide().text("");
 			testPrenom = true;
+			valider();
 		}
 	});	
-
+	
 	$("#ville").keyup(function() {
 		if ($("#ville").val() == "") {
 			$("#ville").css("border-color", "#ff5b5b"); 
-			$("#ville").next(".erreurVille").show().attr('src', 'cross.png');
-			$(".erreurVille").next(".erreur").show().text("Veuillez entrer votre ville");
+			$("#ville").next(".erreur").show().text("Veuillez entrer votre ville");
 			testVille = false; 
-		} else if (!$("#ville").val().match(/^[-a-zA-Zéèç\s]+$/)) {
+			valider();
+		} else if (!$("#ville").val().match(/^[-a-zA-Zéèç]+$/)) {
 			$("#ville").css("border-color", "#ff5b5b"); 
-			$("#ville").next(".erreurVille").show().attr('src', 'cross.png');
-			$(".erreurVille").next(".erreur").show().text("Veuillez entrer une ville valide");	
+			$("#ville").next(".erreur").show().text("Veuillez entrer une ville valide");	
 			testVille = false;
 		} else {
 			$("#ville").css("border-color", "#00ff00"); 
-			$("#ville").next(".erreurVille").show().attr('src', 'tick.png');
-			$(".erreurVille").next(".erreur").hide().text("");
+			$("#ville").next(".erreur").hide().text("");
 			testVille = true;
+			valider();
 		}
 	});	
-
-	$("#dateN").keyup(function() {
-		if ($("#dateN").val() == "") {
-			$("#dateN").css("border-color", "#ff5b5b"); 
-			$("#dateN").next(".erreurDN").show().attr('src', 'cross.png');
-			$(".erreurDN").next(".erreur").show().text("Veuillez renseigner votre date de naissance");
+	
+	$("#dateNaissance").keyup(function() {
+		if ($("#dateNaissance").val() == "") {
+			$("#dateNaissance").css("border-color", "#ff5b5b"); 
+			$("#dateNaissance").next(".erreur").show().text("Veuillez renseigner votre date de naissance");
 			testDate = false; 
+			valider();
 		} else {
-			$("#dateN").css("border-color", "#00ff00"); 
-			$("#dateN").next(".erreurDN").show().attr('src', 'tick.png');
-			$(".erreurDN").next(".erreur").hide().text("");
+			$("#dateNaissance").css("border-color", "#00ff00"); 
+			$("#dateNaissance").next(".erreur").hide().text("");
 			testDate = true;
+			valider();
 		}
 	});	
 		
-	$("#mail").keyup(function() {
-		if ($("#mail").val() == "") {
-			$("#mail").css("border-color", "#ff5b5b"); 
-			$("#mail").next(".erreurEmail").show().attr('src', 'cross.png');
-			$(".erreurEmail").next(".erreur").show().text("Veuillez entrer votre mail");
+	$("#mailI").keyup(function() {
+		if ($("#mailI").val() == "") {
+			$("#mailI").css("border-color", "#ff5b5b"); 
+			$("#mailI").next(".erreur").show().text("Veuillez entrer votre mail");
 			testmail = false; 
-		} else if (!$("#mail").val().match(/^[-a-zA-Z_.]+\@[a-z]+(.)(fr|com)$/)) {
-			$("#mail").css("border-color", "#ff5b5b"); 
-			$("#mail").next(".erreurEmail").show().attr('src', 'cross.png');
-			$(".erreurEmail").next(".erreur").show().text("Veuillez entrer un mail valide");
+			valider();
+		} else if (!$("#mailI").val().match(/^[-a-zA-Z_.]+\@[a-z]+(.)(fr|com)$/)) {
+			$("#mailI").css("border-color", "#ff5b5b"); 
+			$("#mailI").next(".erreur").show().text("Veuillez entrer un mail valide");	
 			testmail = false;
 		} else {
-			$("#mail").css("border-color", "#00ff00"); 
-			$("#mail").next(".erreurEmail").show().attr('src', 'tick.png');
-			$(".erreurEmail").next(".erreur").hide().text("");
+			$("#mailI").css("border-color", "#00ff00"); 
+			$("#mailI").next(".erreur").hide().text("");
 			testMail = true;
+			valider();
 		}
 	});
-
-	$('#password').keyup(function() {
-		if (!$("#password").val().match(/^[a-zA-Z0-9].{5,}$/)) {
-			$("#password").css("border-color", "#ff5b5b");
-			$("#password").next(".erreurMDP1").show().attr('src', 'cross.png');
-			$(".erreurMDP1").next(".erreur").show().text("Mot de passe trop petit");
-		} else if ($('#password').val() == $('#password2').val()) {
-			$('#password').css("border-color", "#00ff00");
-			$('#password2').css("border-color", "#00ff00");
-			$("#password").next(".erreurMDP1").show().attr('src', 'tick.png');
-			$("#password2").next(".erreurMDP2").show().attr('src', 'tick.png');
-			$(".erreurMDP1").next(".erreur").hide().text("");
-			$(".erreurMDP2").next(".erreur").hide().text("");
-			pass = true;
-			if (pass && mail && ville && nom && prenom && dateN) { 
-				$('#submit').removeAttr('disabled');
-			}
+	
+	$("#mdp1").keyup(function() {	
+		if ($("#mdp1").val() == "") {
+			$("#mdp1").css("border-color", "#ff5b5b"); 
+			$("#mdp1").next(".erreur").show().text("Veuillez entrer un mot de passe");
+			testMdp1 = false;
+			$("#mdp2").css("border-color", "#ff5b5b"); 
+			$("#mdp2").next(".erreur").show().text("Mot de passe non identique au premier");
+			testMdp2 = false;
+			valider();
+		} else if (!$("#mdp1").val().match(/^[a-zA-Z0-9].{5,12}$/)) {
+			$("#mdp1").css("border-color", "#ff5b5b"); 
+			$("#mdp1").next(".erreur").show().text("Mot de passe trop petit");	
+			testMdp1 = false;
+			$("#mdp2").css("border-color", "#ff5b5b"); 
+			$("#mdp2").next(".erreur").show().text("Mot de passe non identique au premier");
+			testMdp2 = false;
+			valider();
 		} else {
-			$('#password').css("border-color", "#ff5b5b");
-			$('#password2').css("border-color", "#ff5b5b");
-			$("#password").next(".erreurMDP1").show().attr('src', 'cross.png');
-			$("#password2").next(".erreurMDP2").show().attr('src', 'cross.png');
-			$(".erreurMDP1").next(".erreur").show().text("Le mot de passe n'est pas identique");
-			$(".erreurMDP2").next(".erreur").show().text("Le mot de passe n'est pas identique");
-			$('#submit').attr('disabled', 'true');
-			pass = false;
+			$("#mdp1").css("border-color", "#00ff00"); 
+			$("#mdp1").next(".erreur").hide().text("");
+			testMdp1 = true;
+			if ($("#mdp2").val() == $("#mdp1").val()) {
+				$("#mdp2").css("border-color", "#00ff00"); 
+				$("#mdp2").next(".erreur").hide().text("");
+				testMdp2 = true;
+			}	
+			valider();
 		}
 	});
-
-	$( '#password2' ).keyup(function() {
-		if (!$("#password2").val().match(/^[a-zA-Z0-9].{5,}$/)) {
-			$("#password2").css("border-color", "#ff5b5b"); 
-			$("#password2").next(".erreurMDP2").show().attr('src', 'cross.png');
-			$(".erreurMDP2").next(".erreur").show().text("Mot de passe trop petit");
-		} else if ($('#password').val() == $('#password2').val()) {
-			$( '#password' ).css("border-color", "#00ff00");
-			$( '#password2' ).css("border-color", "#00ff00");
-			$("#password").next(".erreurMDP1").show().attr('src', 'tick.png');
-			$("#password2").next(".erreurMDP2").show().attr('src', 'tick.png');
-			$(".erreurMDP1").next(".erreur").hide().text("");
-			$(".erreurMDP2").next(".erreur").hide().text("");
-			pass = true;
-			if (pass && mail && ville && nom && prenom && dateN) { 
-				$('#submit').removeAttr('disabled');
-			}
-		} else {
-			$('#password').css("border-color", "ff5b5b");
-			$('#password2').css("border-color", "ff5b5b");
-			$("#password").next(".erreurMDP1").show().attr('src', 'cross.png');
-			$("#password2").next(".erreurMDP2").show().attr('src', 'cross.png');
-			$(".erreurMDP2").next(".erreur").hide().text("Le mot de passe n'est pas identique");
-			$('#submit').attr('disabled', 'true');
-			pass = false;
+	
+	$("#mdp2").keyup(function() {	
+		if ($("#mdp2").val() != $("#mdp1").val() || $("#mdp2").val() == "") {
+			$("#mdp2").css("border-color", "#ff5b5b"); 
+			$("#mdp2").next(".erreur").show().text("Mot de passe non identique au premier");
+			testMdp2 = false;
+			valider();
+		} else if ($("#mdp2").val() != "") {
+			$("#mdp2").css("border-color", "#00ff00"); 
+			$("#mdp2").next(".erreur").hide().text("");
+			testMdp2 = true;
+			valider();
 		}
-	});
+	});		
 });
