8e2514815e9b02be0c450717ef3846ad439ac1bc
JeremyTevenin <jeremy_tevenin@hotmail.fr>
2016-12-15 10:40:50 +0100

    bdd menu maj

--- Bases_de_donnees/menu.sql
+++ Bases_de_donnees/menu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 06 Décembre 2016 à 12:32
+-- Généré le :  Jeu 15 Décembre 2016 à 09:36
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -23,16 +23,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `fichier`
+-- Structure de la table `article`
 --
 
-CREATE TABLE `fichier` (
+CREATE TABLE `article` (
+  `id_souscateg` int(11) NOT NULL,
   `id_article` int(11) NOT NULL,
-  `id_fichier` int(11) NOT NULL,
-  `nom_fichier` varchar(50) NOT NULL,
-  `titre_fichier` varchar(50) NOT NULL,
-  `format_fichier` varchar(5) NOT NULL
+  `id_auteur` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `nom_article` varchar(50) NOT NULL,
+  `texte_page` text NOT NULL,
+  `repertoire` varchar(45) NOT NULL,
+  `url` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `article`
+--
+
+INSERT INTO `article` (`id_souscateg`, `id_article`, `id_auteur`, `date`, `nom_article`, `texte_page`, `repertoire`, `url`) VALUES
+(1, 1, 0, '0000-00-00', 'Structure administrative', '', 'structure_administrative', 'structure_administrative.php'),
+(1, 2, 0, '0000-00-00', 'Structure pédagogique', '', 'structure_p_edagogique', 'structure_p_edagogique.php'),
+(1, 3, 0, '0000-00-00', 'Vie scolaire', '', 'vie_scolaire', 'vie_scolaire.php'),
+(2, 4, 0, '0000-00-00', 'Le choix des options', '', 'le_choix_des_options', 'le_choix_des_options.php'),
+(2, 6, 0, '0000-00-00', 'Section européenne allemand', '', 'section_europ_eenne_allemand', 'section_europ_eenne_allemand.php'),
+(4, 13, 0, '0000-00-00', 'Les nouvelles classes de seconde', '', 'les_nouvelles_classes_de_seconde', 'les_nouvelles_classes_de_seconde.php'),
+(5, 14, 0, '0000-00-00', 'Stage d\'observation en 3ème', '', 'stage_d_observation_en_3eme', 'stage_d_observation_en_3eme.php'),
+(5, 15, 0, '0000-00-00', 'L\'option latin', '', 'l_option_latin', 'l_option_latin.php'),
+(6, 16, 0, '0000-00-00', 'Références des oeuvres étudiées', '', 'r_ef_erences_des_oeuvres__etudi_ees', 'r_ef_erences_des_oeuvres__etudi_ees.php'),
+(6, 17, 0, '0000-00-00', 'Horaires histoire des arts', '', 'horaires_histoire_des_arts', 'horaires_histoire_des_arts.php'),
+(7, 18, 0, '0000-00-00', 'Les horaires', '', 'les_horaires', 'les_horaires.php'),
+(7, 19, 0, '0000-00-00', 'Le réglement', '', 'le_r_eglement', 'le_r_eglement.php'),
+(1, 25, 1, '2016-12-14', 'insert null', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -50,49 +72,10 @@ CREATE TABLE `categorie` (
 --
 
 INSERT INTO `categorie` (`id_categ`, `nom_categ`) VALUES
-(1, 'Retouche'),
-(2, 'Fabrication'),
+(1, 'RetouchÃ©'),
 (3, 'Transformation'),
-(4, 'Création'),
-(5, 'Collections été 2015');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `article`
---
-
-CREATE TABLE `article` (
-  `id_souscateg` int(11) NOT NULL,
-  `id_article` int(11) NOT NULL,
-  `nom_article` varchar(50) NOT NULL,
-  `texte_page` text NOT NULL,
-  `repertoire` varchar(45) NOT NULL,
-  `url` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `article`
---
-
-INSERT INTO `article` (`id_souscateg`, `id_article`, `nom_article`, `texte_page`, `repertoire`, `url`) VALUES
-(1, 1, 'Structure administrative', '', 'structure_administrative', 'structure_administrative.php'),
-(1, 2, 'Structure pédagogique', '', 'structure_p_edagogique', 'structure_p_edagogique.php'),
-(1, 3, 'Vie scolaire', '', 'vie_scolaire', 'vie_scolaire.php'),
-(2, 4, 'Le choix des options', '', 'le_choix_des_options', 'le_choix_des_options.php'),
-(2, 6, 'Section européenne allemand', '', 'section_europ_eenne_allemand', 'section_europ_eenne_allemand.php'),
-(3, 7, 'Liste des fournitures', '', 'liste_des_fournitures', 'liste_des_fournitures.php'),
-(3, 8, 'Inscription 5e, 4e et 3e ', '', 'inscription_5e_4e_et_3e_', 'inscription_5e_4e_et_3e_.php'),
-(3, 9, 'Inscription 6e', '', 'inscription_6e', 'inscription_6e.php'),
-(3, 10, 'Modalités de réinscription', '', 'modalit_es_de_r_einscription', 'modalit_es_de_r_einscription.php'),
-(3, 11, 'Inscription en section bilangue', '', 'inscription_en_section_bilangue', 'inscription_en_section_bilangue.php'),
-(4, 13, 'Les nouvelles classes de seconde', '', 'les_nouvelles_classes_de_seconde', 'les_nouvelles_classes_de_seconde.php'),
-(5, 14, 'Stage d\'observation en 3ème', '', 'stage_d_observation_en_3eme', 'stage_d_observation_en_3eme.php'),
-(5, 15, 'L\'option latin', '', 'l_option_latin', 'l_option_latin.php'),
-(6, 16, 'Références des oeuvres étudiées', '', 'r_ef_erences_des_oeuvres__etudi_ees', 'r_ef_erences_des_oeuvres__etudi_ees.php'),
-(6, 17, 'Horaires histoire des arts', '', 'horaires_histoire_des_arts', 'horaires_histoire_des_arts.php'),
-(7, 18, 'Les horaires', '', 'les_horaires', 'les_horaires.php'),
-(7, 19, 'Le réglement', '', 'le_r_eglement', 'le_r_eglement.php');
+(4, 'CrÃ©ation modif rÃ©ussi'),
+(5, 'Collections de l\'Ã©tÃ© 2016');
 
 -- --------------------------------------------------------
 
@@ -111,30 +94,17 @@ CREATE TABLE `souscategorie` (
 --
 
 INSERT INTO `souscategorie` (`id_categ`, `id_souscateg`, `nom_souscateg`) VALUES
-(1, 1, 'Les équipes    '),
-(1, 2, 'Les options et spécificités'),
-(2, 3, 'Inscription 2014-2015'),
-(3, 4, 'Après la 3ème'),
-(3, 5, 'Pendant le collège'),
+(1, 1, 'Les Ã©quipes    '),
+(1, 2, 'Les options et spÃ©cificitÃ©s'),
+(3, 4, 'AprÃ¨s la 3Ã¨me'),
+(3, 5, 'Pendant le collÃ¨ge'),
 (4, 6, 'Histoire des arts 2013-2014'),
-(5, 7, 'Le fonctionnement du CDI');
+(5, 7, 'Le fonctionnement du CDI'),
+(1, 9, 'jkl-vr fr');
 
 --
 -- Index pour les tables exportées
 --
-
---
--- Index pour la table `fichier`
---
-ALTER TABLE `fichier`
-  ADD PRIMARY KEY (`id_fichier`),
-  ADD KEY `id_article` (`id_article`);
-
---
--- Index pour la table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`id_categ`);
 
 --
 -- Index pour la table `article`
@@ -142,6 +112,12 @@ ALTER TABLE `categorie`
 ALTER TABLE `article`
   ADD PRIMARY KEY (`id_article`,`nom_article`),
   ADD KEY `id_souscateg` (`id_souscateg`);
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id_categ`);
 
 --
 -- Index pour la table `souscategorie`
@@ -155,34 +131,23 @@ ALTER TABLE `souscategorie`
 --
 
 --
--- AUTO_INCREMENT pour la table `fichier`
+-- AUTO_INCREMENT pour la table `article`
 --
-ALTER TABLE `fichier`
-  MODIFY `id_fichier` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `article`
+  MODIFY `id_article` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `categorie`
   MODIFY `id_categ` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT pour la table `article`
---
-ALTER TABLE `article`
-  MODIFY `id_article` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
---
 -- AUTO_INCREMENT pour la table `souscategorie`
 --
 ALTER TABLE `souscategorie`
-  MODIFY `id_souscateg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_souscateg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- Contraintes pour les tables exportées
 --
-
---
--- Contraintes pour la table `fichier`
---
-ALTER TABLE `fichier`
-  ADD CONSTRAINT `fichier_ibfk_1` FOREIGN KEY (`id_article`) REFERENCES `article` (`id_article`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `article`
